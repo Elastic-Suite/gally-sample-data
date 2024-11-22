f8a725ac8b2a7e6fd3ad60e6b94faf5d04e19c82
Botis <botis@smile.fr>
2024-11-22 17:31:10 +0100

    fix: Premium fixtures are launched all time

--- src/Locator/PremiumFilesLocator.php
+++ src/Locator/PremiumFilesLocator.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace Gally\SampleData\Locator;
 
 use Gally\Bundle\State\ExtraBundleProvider;
+use Gally\SampleData\GallySampleDataBundle;
 use Hautelook\AliceBundle\FixtureLocatorInterface;
 use Nelmio\Alice\IsAServiceTrait;
 
@@ -30,13 +31,26 @@ final class PremiumFilesLocator implements FixtureLocatorInterface
     public function locateFiles(array $bundles, string $environment): array
     {
         $files = $this->decoratedFixtureLocator->locateFiles($bundles, $environment);
-
         // If premium bundles are not installed, we do not run premium fixtures
-        if (empty($this->extraBundleProvider->get())) {
+        if (empty($this->getExtraBundlesCleaned())) {
             $files = array_filter($files, [$this, 'filterPremiumFixtures']);
         }
 
         return $files;
+    }
+
+    protected function getExtraBundlesCleaned(): array
+    {
+        $sampleDataBundleName = explode('\\', GallySampleDataBundle::class);
+        $sampleDataBundleName = array_pop($sampleDataBundleName);
+
+        $extraBundles = $this->extraBundleProvider->get();
+        $sampleDataBundleKey = array_search($sampleDataBundleName, array_column($extraBundles, 'name'), true);
+        if (false !== $sampleDataBundleKey) {
+            unset($extraBundles[$sampleDataBundleKey]);
+        }
+
+        return $extraBundles;
     }
 
     private function filterPremiumFixtures(string $file): bool
